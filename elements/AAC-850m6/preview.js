2dfccafd2a9ae1be1cb23191dadced28d09d2583
Hassen KHROUF <105175162+hassenkhrouf@users.noreply.github.com>
2023-08-11 17:33:43 +0100

    Automatic commit from Bubble

---
+++ elements/AAC-850m6/preview.js
@@ -0,0 +1,6 @@
+function(instance, properties) {
+
+let el =$('<div><img src="//meta-l.cdn.bubble.io/f1691770706000x200852787617585180/blue-snow-icon-8.png"></div>');
+instance.canvas.append(el);
+
+}
